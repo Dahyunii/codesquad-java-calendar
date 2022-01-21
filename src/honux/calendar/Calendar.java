2c37e54e3535acdb1d043aade8e8ac442ceed35a
Dahyun <Dahyun@localhost>
2022-01-21 23:29:51 +0900

    Changing the existing Calendar class to new Calendar class that calls a specific month.

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class Calendar {
 	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getmaxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month -1];
 	}
 	
-	public void printSampleCalendar() {
-		System.out.println(" 월 화 수 목 금 토");
-		System.out.println("--------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+	public void printCalendar(int year, int month) { 
+		System.out.printf("   <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("----------------------");
+		
+		int maxDay = getMaxDaysOfMonth(month);
+		
+		for(int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
 		
 	}
 	
@@ -35,52 +47,6 @@ public class Calendar {
 		sc.close();
 	}
 	
-	public static void main(String[] args) {
 
-		// (강의)숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		String PROMPT = "cal> ";	// > 뒤 공백 있어야 함
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		
-		int month = 0;
-		
-		while(true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if(month == 0) {
-				break;
-			}else if(month > 12 | month < 0) {
-				System.out.println("1월부터 12월까지만 입력이 가능합니다.");
-				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getmaxDaysOfMonth(month));
-		}
-		
-		System.out.println("반복을 종료합니다.");
-		scanner.close();
-		
-		
-//		System.out.println("반복 횟수를 입력하세요.");
-//		int repeat = scanner.nextInt();
-//		int month = 0;
-//		
-//		if(month > 0 | month < 13) {
-//			for(int i = 0; i < repeat; i++) {
-//				System.out.println("달을 입력하세요.");
-//				month = scanner.nextInt();
-//				System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getmaxDaysOfMonth(month));
-//			}
-//			System.out.println("반복을 종료합니다.");
-//		}else {
-//			System.out.println("1월부터 12월까지만 입력이 가능합니다.");
-//		}
-
-//		cal.madeByDahyunMaxCalendar();
-		// >> public void 로만 작성한 거를 부를 때 사용하는 방법.
-
-//		scanner.close();
-		
-	}
 
 }
