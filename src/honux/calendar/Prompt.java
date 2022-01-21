ddbfae571675e0f2abd5f9af6c6b9954a2bd42d9
Dahyun <Dahyun@localhost>
2022-01-22 00:23:40 +0900

    Adding Leap Year to the existing Calendar.

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT = "cal> ";
-
+	private final static String PROMPT_Y = "YEAR> ";
+	private final static String PROMPT_M = "MONTH> ";
+	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int month = 0;
-
+		int year = 0;
+		
 		while (true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.print(PROMPT_Y);
+			year = scanner.nextInt();
 			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print(PROMPT_M);
 			month = scanner.nextInt();
-			if (month == 0) {
+			
+			if (year == 0 || month == 0) {
 				break;
 			} else if (month > 12 | month < 0) {
 				System.out.println("1월부터 12월까지만 입력이 가능합니다.");
 				continue;
 			}
-			cal.printCalendar(2022, month);
+			cal.printCalendar(year, month);
 		}
 
 		System.out.println("반복을 종료합니다.");
